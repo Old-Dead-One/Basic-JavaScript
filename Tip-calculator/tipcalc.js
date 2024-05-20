9bda8467912a85c9c28b2a7ab0af396a979128be
Alan McGraw <alanmcgraw@proton.me>
2024-05-20 11:31:59 -0600

    Basic tip clculator

---
+++ Tip-calculator/tipcalc.js
@@ -0,0 +1,7 @@
+var bill = Number(prompt("What is the total bill? $"));
+var tip = Number(prompt("Enter the tip percentage: %"));
+
+var tipAmount = bill * (tip / 100);
+var totalBill = tip_amount + bill;
+
+alert ("The total bill with tip is: " + totalBill.toFixed(2));
